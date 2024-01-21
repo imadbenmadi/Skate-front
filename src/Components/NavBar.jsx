a17f8bba77d09fb176ca9612b361bd5a1a091d23
imadbenmadi <imadbenmadi891@gmail.com>
2024-01-21 23:14:41 +0100

    Add Tailwind CSS and update NavBar component

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
-
+import React from "react";
+import Logo from "../../public/Logo.png";
+import Setting from "../../public/setting.png";
 function NavBar() {
-  return (
-    <div>NavBar</div>
-  )
+    return (
+        <div className=" flex ">
+            <div>
+                <img src={Logo} alt="Logo" />
+            </div>
+            <div className=" flex gap-5">
+                <div>Servecis</div>
+                <div>Formations</div>
+                <div>Events</div>
+                <div>Blogs</div>
+                <div>Contact</div>
+            </div>
+            <div>
+          <div>
+            <img src={Setting} alt="" />
+                </div>
+                <div></div>
+            </div>
+        </div>
+    );
 }
 
-export default NavBar
+export default NavBar;
