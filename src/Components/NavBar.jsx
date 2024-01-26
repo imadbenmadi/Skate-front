f0f101e8d502d3fe894858a7f8a57fe592047e8f
imadbenmadi <imadbenmadi891@gmail.com>
2024-01-26 17:55:23 +0100

    Update NavBar component with responsive design

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,24 +1,32 @@
 import React from "react";
 import Logo from "../../public/Logo.png";
 import Setting from "../../public/setting.png";
+import { IoMenu } from "react-icons/io5";
+import { IoClose } from "react-icons/io5";
+
 function NavBar() {
     return (
-        <div className=" flex ">
-            <div>
-                <img src={Logo} alt="Logo" />
+        <div className=" flex shadow-lg justify-between ">
+            <div className=" p-2">
+                <img src={Logo} alt="Logo" className=" w-20 " />
             </div>
-            <div className=" flex gap-5">
-                <div>Servecis</div>
-                <div>Formations</div>
-                <div>Events</div>
-                <div>Blogs</div>
-                <div>Contact</div>
+            <div className=" md:hidden flex flex-col items-center justify-center mr-10">
+                <IoMenu className=" text-5xl text-gray-500	" />
             </div>
-            <div>
-          <div>
-            <img src={Setting} alt="" />
+            <div className="  hidden md:flex">
+                <div className=" flex gap-5">
+                    <div className=" ">Servecis</div>
+                    <div>Formations</div>
+                    <div>Events</div>
+                    <div>Blogs</div>
+                    <div>Contact</div>
                 </div>
-                <div></div>
+                <div>
+                    <div>
+                        <img src={Setting} alt="" />
+                    </div>
+                    <div></div>
+                </div>
             </div>
         </div>
     );
