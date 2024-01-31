b46853d5200d5a7be4f65bce7b0a799806e26e58
imadbenmadi <imadbenmadi891@gmail.com>
2024-01-31 13:57:42 +0100

    Add Pages_Btns component to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import NavBar from "./Components/NavBar";
 import { Outlet } from "react-router";
-
+import Pages_Btns from "./Components/Pages_Btns";
 function App() {
   return (
       <div className=" relative overflow-x-hidden ">
           <NavBar />
-      <div className=" mt-[70px] md:mt-[92px]">
+          <Pages_Btns />
+
+          <div className=" mt-[70px] md:mt-[92px]">
               <Outlet />
           </div>
       </div>
---
+++ src/Components/Pages_Btns.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Pages_Btns() {
+  return (
+      <div className=" fixed bottom-2/3 md:bottom-1/2 left-5 flex flex-col items-center gap-4">
+          <div className="  bg-gray w-5 h-5 rounded-full "> </div>
+          <div className=" bg-gray w-5 h-5 rounded-full "> </div>
+          <div className=" bg-gray w-5 h-5 rounded-full "> </div>
+          <div className=" bg-gray w-5 h-5 rounded-full "> </div>
+      </div>
+  );
+}
+
+export default Pages_Btns
