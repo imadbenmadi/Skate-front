90d1fb00bfb3d8b4af0416a939e557cda9480ea2
imadbenmadi <imadbenmadi891@gmail.com>
2024-01-31 13:45:52 +0100

    Refactor Home component to use Hero component

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,42 +1,11 @@
 import React from "react";
-import wallpaper from "../../../public/wallpaper.jpg";
-import { GrFormNextLink } from "react-icons/gr";
 
+import Hero from "./Hero";
 function Home() {
     return (
-        <>
-            <div>
-                <div className=" h-[50vh] md:h-[90vh] overflow-hidden">
-                    {/* Set the image as a background */}
-                    <div
-                        className="w-full h-full bg-cover bg-center  flex  justify-center items-end gap-10  md:gap-64 pb-10"
-                        style={{
-                            backgroundImage: `url(${wallpaper})`,
-                            backgroundSize: "cover",
-                        }}
-                    >
-                        <div className="flex flex-col items-end md:items-start w-[200px]">
-                            <div className="hidden md:block  text-2xl mb-3 font-semibold text-black_text">
-                                Center of Study Support & Consultation
-                            </div>
-                            <div className=" flex items-center text-[16px]  bg-green text-white rounded-xl shadow-xl w-fit px-4 py-1 md:text-xl cursor-pointer">
-                                Owr Services
-                                <GrFormNextLink />
-                            </div>
-                        </div>
-                        <div className="w-[200px]">
-                            <div className="hidden md:block text-2xl mb-3 font-semibold text-black_text">
-                                Center of Training and Assistance
-                            </div>
-                            <div className=" flex items-center text-[16px] bg-white text-black rounded-xl shadow-xl w-fit px-4 py-1 md:text-xl cursor-pointer font-semibold">
-                                Owr Courses
-                                <GrFormNextLink />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </>
+        <div>
+            <Hero />
+        </div>
     );
 }
 
