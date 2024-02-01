f1f6ae8b0e7e806beb9b5c05383c78d4a2236dc6
khenfer salah eddine <107678289+salahkhenfer@users.noreply.github.com>
2024-02-01 10:58:49 +0100

    add some fixes from salah

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -5,195 +5,165 @@ import { Link } from "react-router-dom";
 import Axios from "axios";
 import Swal from "sweetalert2";
 function Register() {
-    async function handleRegistration(
+  async function handleRegistration(values, { setSubmitting }) {
+    console.log("test");
+    try {
+      let response = await Axios.post(
+        "http://localhost:3000/Register",
         values,
-        { setSubmitting, setFieldValue }
-    ) {
-        
-        console.log("test");
-        try {
-            let response = await Axios.post(
-                "http://localhost:3000/Register",
-                values,
-                {
-                    withCredentials: true,
-                    // headers: {
-                    //     "Content-Type": "multipart/form-data",
-                    // },
-                    validateStatus: () => true,
-                }
-            );
-
-            if (response.status === 401) {
-                Swal.fire(
-                    "Email already exists",
-                    "Please try to use another Email",
-                    "error"
-                );
-            } else if (response.status === 200) {
-                Swal.fire(
-                    "Done!",
-                    "Your account has been created Successfully",
-                    "success"
-                );
-            } else if (response.status === 400) {
-                Swal.fire(
-                    "Error!",
-                    "Internal server error. Please try again",
-                    "error"
-                );
-            } else if (response.status === 409) {
-                Swal.fire("Error!", "Missing Data", "error");
-            } else {
-                Swal.fire(
-                    "Error!",
-                    "Something Went Wrong. Please try again",
-                    "error"
-                );
-            }
-        } catch (error) {
-            console.error("Error during registration:", error);
-            Swal.fire(
-                "Error!",
-                "Something Went Wrong. Please try again",
-                "error"
-            );
+        {
+          withCredentials: true,
+          // headers: {
+          //     "Content-Type": "multipart/form-data",
+          // },
+          validateStatus: () => true,
         }
-        console.log("erztrfsdf");
-        setSubmitting(false);
+      );
+
+      if (response.status === 401) {
+        Swal.fire(
+          "Email already exists",
+          "Please try to use another Email",
+          "error"
+        );
+      } else if (response.status === 200) {
+        Swal.fire(
+          "Done!",
+          "Your account has been created Successfully",
+          "success"
+        );
+      } else if (response.status === 400) {
+        Swal.fire("Error!", "Internal server error. Please try again", "error");
+      } else if (response.status === 409) {
+        Swal.fire("Error!", "Missing Data", "error");
+      } else {
+        Swal.fire("Error!", "Something Went Wrong. Please try again", "error");
+      }
+    } catch (error) {
+      console.error("Error during registration:", error);
+      Swal.fire("Error!", "Something Went Wrong. Please try again", "error");
     }
-    return (
-        <div>
-            <div>
-                <img className=" w-20 m-auto pt-5 " src={Logo} alt="" />
-            </div>
-            <div className=" m-auto text-center pt-5 text-2xl font-semibold text-blue ">
-                Join to Skate Community
-            </div>
-            {/* input fields */}
-            <div className=" border border-gray_white text-black_text shadow-md w-[80%] md:w-[60%] m-auto mt-3 p-5 rounded-lg  ">
-                <div className=" text-lg font-semibold mb-4 ">
-                    Create Your Account
-                </div>
+    console.log("erztrfsdf");
+    setSubmitting(false);
+  }
+  return (
+    <div>
+      <div>
+        <img className=" w-20 m-auto pt-5 " src={Logo} alt="" />
+      </div>
+      <div className=" m-auto text-center pt-5 text-2xl font-semibold text-blue ">
+        Join to Skate Community
+      </div>
+      {/* input fields */}
+      <div className=" border border-gray_white text-black_text shadow-md w-[80%] md:w-[60%] m-auto mt-3 p-5 rounded-lg  ">
+        <div className=" text-lg font-semibold mb-4 ">Create Your Account</div>
 
-                <Formik
-                    initialValues={{
-                        firstName: "",
-                        lastName: "",
-                        email: "",
-                        password: "",
-                        age: 0,
-                        gender: "",
-                    }}
-                    validate={(values) => {
-                        const errors = {};
-                        // Validate First Name
-                        if (!values.firstName) {
-                            errors.firstName = "Required";
-                        }
+        <Formik
+          initialValues={{
+            firstName: "",
+            lastName: "",
+            email: "",
+            password: "",
+          }}
+          validate={(values) => {
+            const errors = {};
+            // Validate First Name
+            if (!values.firstName) {
+              errors.firstName = "Required";
+            }
 
-                        // Validate Last Name
-                        if (!values.lastName) {
-                            errors.lastName = "Required";
-                        }
+            // Validate Last Name
+            if (!values.lastName) {
+              errors.lastName = "Required";
+            }
 
-                        // Validate Email
-                        if (!values.email) {
-                            errors.email = "Required";
-                        } else if (
-                            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(
-                                values.email
-                            )
-                        ) {
-                            errors.email = "Invalid email address";
-                        }
+            // Validate Email
+            if (!values.email) {
+              errors.email = "Required";
+            } else if (
+              !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+            ) {
+              errors.email = "Invalid email address";
+            }
 
-                        // Validate Password
-                        if (!values.password) {
-                            errors.password = "Required";
-                        } else if (values.password.length < 8) {
-                            errors.password =
-                                "Password must be at least 8 characters long";
-                        }
+            // Validate Password
+            if (!values.password) {
+              errors.password = "Required";
+            } else if (values.password.length < 8) {
+              errors.password = "Password must be at least 8 characters long";
+            }
 
-                        // Validate Age
-                        if (!values.age) {
-                            errors.age = "Required";
-                        } else if (isNaN(values.age) || values.age <= 0) {
-                            errors.age = "Invalid age";
-                        }
+            //     // Validate Age
+            //     if (!values.age) {
+            //       errors.age = "Required";
+            //     } else if (isNaN(values.age) || values.age <= 0) {
+            //       errors.age = "Invalid age";
+            //     }
 
-                        // Validate Gender
-                        if (!values.gender) {
-                            errors.gender = "Required";
-                        }
-                        return errors;
-                    }}
-                    onSubmit={handleRegistration}
-                >
-                    {({ isSubmitting }) => (
-                        <Form className=" flex flex-col ml-5 gap-4">
-                            <div>
-                                <div>First Name</div>
-                                <Field
-                                    type="text"
-                                    name="firstName"
-                                    className="border border-gray_white px-2 py-1 rounded  shadow-sm"
-                                    disabled={isSubmitting}
-                                />
-                                <ErrorMessage
-                                    name="firstName"
-                                    component="div"
-                                />
-                            </div>
-                            <div>
-                                <div>Last Name</div>
-                                <Field
-                                    type="text"
-                                    name="lastName"
-                                    disabled={isSubmitting}
-                                    className="border border-gray_white px-2 py-1 rounded  shadow-sm"
-                                />
-                                <ErrorMessage name="lastName" component="div" />
-                            </div>
-                            <div>
-                                <div>Email</div>
-                                <Field
-                                    type="email"
-                                    name="email"
-                                    disabled={isSubmitting}
-                                    className="border border-gray_white px-2 py-1 rounded  shadow-sm"
-                                />
-                                <ErrorMessage name="email" component="div" />
-                            </div>
-                            <div>
-                                <div>Password</div>
-                                <Field
-                                    type="password"
-                                    name="password"
-                                    disabled={isSubmitting}
-                                    className="border border-gray_white px-2 py-1 rounded  shadow-sm"
-                                />
-                                <ErrorMessage name="password" component="div" />
-                            </div>
+            //     // Validate Gender
+            //     if (!values.gender) {
+            //       errors.gender = "Required";
+            //     }
+            return errors;
+          }}
+          onSubmit={handleRegistration}
+        >
+          {({ isSubmitting }) => (
+            <Form className=" flex flex-col ml-5 gap-4">
+              <div>
+                <div>First Name</div>
+                <Field
+                  type="text"
+                  name="firstName"
+                  className="border border-gray_white px-2 py-1 rounded  shadow-sm"
+                  disabled={isSubmitting}
+                />
+                <ErrorMessage name="firstName" component="div" />
+              </div>
+              <div>
+                <div>Last Name</div>
+                <Field
+                  type="text"
+                  name="lastName"
+                  disabled={isSubmitting}
+                  className="border border-gray_white px-2 py-1 rounded  shadow-sm"
+                />
+                <ErrorMessage name="lastName" component="div" />
+              </div>
+              <div>
+                <div>Email</div>
+                <Field
+                  type="email"
+                  name="email"
+                  disabled={isSubmitting}
+                  className="border border-gray_white px-2 py-1 rounded  shadow-sm"
+                />
+                <ErrorMessage name="email" component="div" />
+              </div>
+              <div>
+                <div>Password</div>
+                <Field
+                  type="password"
+                  name="password"
+                  disabled={isSubmitting}
+                  className="border border-gray_white px-2 py-1 rounded  shadow-sm"
+                />
+                <ErrorMessage name="password" component="div" />
+              </div>
 
-                            <button
-                                type="submit"
-                                className=" bg-green w-fit m-auto px-4 py-2 rounded font-semibold text-white"
-                                disabled={isSubmitting}
-                            >
-                                {isSubmitting ? (
-                                    <div >loading</div>
-                                ) : (
-                                    "Submit"
-                                )}
-                            </button>
-                        </Form>
-                    )}
-                </Formik>
-            </div>
-        </div>
-    );
+              <button
+                type="submit"
+                className=" bg-green w-fit m-auto px-4 py-2 rounded font-semibold text-white"
+                disabled={isSubmitting}
+              >
+                {isSubmitting ? <div>loading</div> : "Submit"}
+              </button>
+            </Form>
+          )}
+        </Formik>
+      </div>
+    </div>
+  );
 }
 
 export default Register;
