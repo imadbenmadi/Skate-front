43ba0722d6182ed53296e49bf816c9d23d1cb66d
imadbenmadi <imadbenmadi891@gmail.com>
2024-02-02 16:17:42 +0100

    edditting margin top

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,15 @@ import NavBar from "./Components/NavBar";
 import { Outlet } from "react-router";
 import Pages_Btns from "./Components/Pages_Btns";
 function App() {
-  return (
-      <div className=" relative overflow-x-hidden ">
-          <NavBar />
-          
+    return (
+        <div className=" relative overflow-x-hidden ">
+            <NavBar />
 
-          <div className=" mt-[70px] md:mt-[92px]">
-              <Outlet />
-          </div>
-      </div>
-  );
+            <div className=" mt-[63px] ">
+                <Outlet />
+            </div>
+        </div>
+    );
 }
 
 export default App;
