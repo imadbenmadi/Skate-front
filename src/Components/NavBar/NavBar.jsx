c1b8b86ecbc44f8848bcc0939d19c72eedae6ad6
imadbenmadi <imadbenmadi891@gmail.com>
2024-02-07 21:04:56 +0100

    Refactor NavBar component and delete Menu_Toggler component

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,19 +1,13 @@
 import React from "react";
 import Logo from "../../../public/Logo.png";
 import { useState, useEffect } from "react";
-import { IoSettingsOutline } from "react-icons/io5";
-import { FaRegHandshake } from "react-icons/fa";
-import { FaBookReader } from "react-icons/fa";
-import { MdEventAvailable } from "react-icons/md";
-import { RiArticleFill } from "react-icons/ri";
-import { IoCall } from "react-icons/io5";
 import { Link } from "react-router-dom";
 import { useAppContext } from "../../Context/AppContext";
 import { useLocation } from "react-router";
-
 import axios from "axios";
-import Menu_Toogler from "./Menu_Toogler";
+import Menu_Toogler from "./Mobile/Menu_Toogler";
 import Laptop_Nav_Items from "./Laptop/Laptop_Nav_Items";
+import Mobile_Nav_Items from "./Mobile/Mobile_Nav_Items";
 function NavBar({ Active_nav, setActive_nav }) {
     const { isAuth, FirstName, LastName, _id } = useAppContext();
     const { set_Auth, store_login } = useAppContext();
@@ -123,87 +117,10 @@ function NavBar({ Active_nav, setActive_nav }) {
                 />
             </div>
             {/* Moblie nav bar */}
-            <div className="flex">
-                <div
-                    onClick={Toogle_Menu_Bar}
-                    className={` md:hidden ${
-                        MobileNav_Open ? " block bg-gray_white " : "hidden"
-                    } absolute  transition-all select-none w-[30vw]  z-50 h-screen  opacity-[0.6] `}
-                ></div>
-                <div
-                    className={` md:hidden ${
-                        MobileNav_Open
-                            ? " translate-x-[30vw]"
-                            : " translate-x-[100vw] "
-                    } absolute  transition-all select-none w-[70vw]  z-50 bg-zinc-100 border-l-4  text-black_text font-semibold `}
-                >
-                    <div className=" flex flex-col items-center justify-start h-screen text-2xl gap-5 mt-8 ">
-                        <div className="flex gap-2 cursor-pointer ">
-                            <Link
-                                to={"/Login"}
-                                className=" bg-green text-[#fff] px-3 py-2 rounded-lg cursor-pointer"
-                            >
-                                Login
-                            </Link>
-                            <Link
-                                to={"/Register"}
-                                className=" bg-gray_white text-black_text px-3 py-2 rounded-lg cursor-pointer"
-                            >
-                                Register
-                            </Link>
-                        </div>
-                        <Link
-                            to={"/Settings"}
-                            className="w-[155px] flex cursor-pointer gap-2  "
-                        >
-                            <IoSettingsOutline className=" text-3xl" />
-                            Settings
-                        </Link>
-                        <div className=" w-full h-1 bg-gray"></div>
-                        <Link
-                            onClick={Toogle_Menu_Bar}
-                            to={"/Services"}
-                            className="w-[155px] flex cursor-pointer gap-2  "
-                        >
-                            <FaRegHandshake className=" text-3xl" />
-                            Services
-                        </Link>
-                        <Link
-                            onClick={Toogle_Menu_Bar}
-                            to={"/Courses"}
-                            className="w-[165px] flex cursor-pointer  gap-2 "
-                        >
-                            <FaBookReader className=" text-3xl" />
-                            Courses
-                        </Link>
-                        <div className=" w-full h-1 bg-gray"></div>
-                        <Link
-                            onClick={Toogle_Menu_Bar}
-                            to={"/Events"}
-                            className="w-[155px] flex cursor-pointer  gap-2"
-                        >
-                            <MdEventAvailable className=" text-3xl" />
-                            Events
-                        </Link>
-                        <Link
-                            onClick={Toogle_Menu_Bar}
-                            to={"/Blogs"}
-                            className="w-[155px] flex cursor-pointer  gap-2"
-                        >
-                            <RiArticleFill className=" text-3xl" />
-                            Blogs
-                        </Link>
-                        <Link
-                            onClick={Toogle_Menu_Bar}
-                            to={"/Contact"}
-                            className="w-[155px] flex cursor-pointer  gap-2"
-                        >
-                            <IoCall className=" text-3xl" />
-                            Contact
-                        </Link>
-                    </div>
-                </div>
-            </div>
+            <Mobile_Nav_Items
+                MobileNav_Open={MobileNav_Open}
+                Toogle_Menu_Bar={Toogle_Menu_Bar}
+            />
         </div>
     );
 }
