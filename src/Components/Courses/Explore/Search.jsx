227245a3b0bbcd82505ef32036476ad681305c52
imadbenmadi <imadbenmadi891@gmail.com>
2024-02-13 12:46:46 +0100

    init the search component

--- src/Components/Courses/Explore/Search.jsx
+++ src/Components/Courses/Explore/Search.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { IoSearchOutline } from "react-icons/io5";
 
-function Search() {
-  return (
-    <div>Search</div>
-  )
+function Search({ setSearch }) {
+  const handleSearch = () => {
+      const searchInput = document.getElementById("searchInput");
+      if (searchInput) {
+          setSearch(searchInput.value);
+    }
+    console.log(searchInput.value);
+  };
+    return (
+        <div className=" flex border ">
+            <input type="text" className=" pl-2" id="searchInput" />
+            <button className=" px-2 border-l" onClick={handleSearch}>
+                <IoSearchOutline />
+            </button>
+        </div>
+    );
 }
 
 export default Search
