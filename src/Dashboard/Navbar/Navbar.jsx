13e63466eec50a7e15d56f55492d996923d49f44
imadbenmadi <imadbenmadi891@gmail.com>
2024-02-15 20:26:49 +0100

    initlize Dashboard Navbar

--- src/Dashboard/Navbar/Navbar.jsx
+++ src/Dashboard/Navbar/Navbar.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 import logo from "../../../public/skate_circle.png";
+import { MdSpaceDashboard } from "react-icons/md";
+import { HiMiniUsers } from "react-icons/hi2";
+import { FaBook } from "react-icons/fa";
+import { FaHandshake } from "react-icons/fa6";
+import { FaCalendarCheck } from "react-icons/fa6";
+import { IoIosPaper } from "react-icons/io";
+
 function Navbar() {
     return (
         <div className=" w-full h-full">
@@ -9,14 +16,34 @@ function Navbar() {
                 </div>
                 <div>Skate</div>
             </div>
+            <div className=" w-full h-[1px] bg-white my-6"></div>
             {/* nav items */}
 
-            <div className="  w-[80%] m-auto text-white" >
-                <div>Dashboarsssssssssssssssssssd</div>
-                <div>Dashboard</div>
-                <div>Dashboard</div>
-                <div>Dashboard</div>
-                <div>Dashboard</div>
+            <div className="  md:w-[80%] m-auto text-white text-sm md:text-xl  lg:text-2xl flex flex-col gap-10 mt-8">
+                <div className=" flex items-center  cursor-pointer">
+                    <MdSpaceDashboard  />
+                    <div>Dashboard</div>
+                </div>
+                <div className=" flex items-center gap-3 cursor-pointer">
+                    <HiMiniUsers />
+                    <div>Users</div>
+                </div>
+                <div className=" flex items-center gap-3 cursor-pointer">
+                    <FaHandshake />
+                    <div>Services</div>
+                </div>
+                <div className=" flex items-center gap-3 cursor-pointer">
+                    <FaBook />
+                    <div>Courses</div>
+                </div>
+                <div className=" flex items-center gap-3 cursor-pointer">
+                    <FaCalendarCheck />
+                    <div>Events</div>
+                </div>
+                <div className=" flex items-center gap-3 cursor-pointer">
+                    <IoIosPaper />
+                    <div>Blogs</div>
+                </div>
             </div>
         </div>
     );
