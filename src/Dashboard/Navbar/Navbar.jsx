0ac6fe1cb06dc6610a2ebdb9605290c7df3b2198
imadbenmadi <imadbenmadi891@gmail.com>
2024-02-15 20:29:20 +0100

    adding some touch to laptop navbar

--- src/Dashboard/Navbar/Navbar.jsx
+++ src/Dashboard/Navbar/Navbar.jsx
@@ -6,20 +6,22 @@ import { FaBook } from "react-icons/fa";
 import { FaHandshake } from "react-icons/fa6";
 import { FaCalendarCheck } from "react-icons/fa6";
 import { IoIosPaper } from "react-icons/io";
-
+import { useLocation } from "react-router-dom";
 function Navbar() {
+    const location = useLocation();
+
     return (
         <div className=" w-full h-full">
-            <div className=" flex gap-3 items-center text-4xl text-green justify-center pt-4 ">
+            <div className=" flex gap-3 items-center text-xl md:text-4xl text-green justify-center pt-4 ">
                 <div>
-                    <img src={logo} alt="logo" className=" w-12" />
+                    <img src={logo} alt="logo" className=" w-8 md:w-12" />
                 </div>
                 <div>Skate</div>
             </div>
             <div className=" w-full h-[1px] bg-white my-6"></div>
             {/* nav items */}
 
-            <div className="  md:w-[80%] m-auto text-white text-sm md:text-xl  lg:text-2xl flex flex-col gap-10 mt-8">
+            <div className=" w-[95%]  md:w-[80%] m-auto text-white text-sm md:text-xl  lg:text-2xl flex flex-col gap-10 mt-8">
                 <div className=" flex items-center  cursor-pointer">
                     <MdSpaceDashboard  />
                     <div>Dashboard</div>
