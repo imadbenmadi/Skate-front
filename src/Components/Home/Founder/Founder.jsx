7962e88a93af209a59fc5224787830c945b9e622
imadbenmadi <imadbenmadi891@gmail.com>
2024-02-24 16:50:30 +0100

    fixing founder responsive

--- src/Components/Home/Founder/Founder.jsx
+++ src/Components/Home/Founder/Founder.jsx
@@ -14,7 +14,7 @@ export default function Founder() {
                     transition:
                         "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.1s",
                 }}
-                className="flex gap-5 max-md:flex-col max-md:gap-0 max-md:"
+                className="flex gap-5  flex-col items-center max-md:gap-0 max-md:"
             >
                 <div className="flex flex-col w-6/12 max-md:ml-0 max-md:w-full">
                     <div className="flex flex-col grow p-4 font-bold max-md:mt-4">
@@ -31,11 +31,11 @@ export default function Founder() {
                     />
                 </div>
                 <div className="flex flex-col w-6/12 max-md:ml-0 max-md:w-full">
-                    <div className=" self-center mt-10 text-2xl text-emerald-500">
+                    <div className=" self-center mt-4 text-2xl text-emerald-500">
                         Fouzia{" "}
                         <span className="text-blue-950 text-gray">Benmadi</span>
                     </div>
-                    <div className=" px-4 mt-11 text-xl font-medium leading-7 text-gray max-md:mt-4">
+                    <div className=" px-4 mt-4 text-xl font-medium leading-7 text-gray max-md:mt-4">
                         In light of the current technological and scientific
                         revolution, choosing a suitable trainer or instructor
                         now requires difficult and sometimes complicated
