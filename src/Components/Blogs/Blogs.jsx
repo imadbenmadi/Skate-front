4fa45111ae419e649b5eba975690ccd41d2d09e2
imadbenmadi <imadbenmadi891@gmail.com>
2024-02-24 16:44:13 +0100

    fixing Blog Component

--- src/Components/Blogs/Blogs.jsx
+++ src/Components/Blogs/Blogs.jsx
@@ -1,30 +1,29 @@
-import React from "react";
-import { useState, useEffect } from "react";
-import { IoSearchOutline } from "react-icons/io5";
+import React, { useState, useEffect } from "react";
+import { IoSearchOutline, IoWarning } from "react-icons/io5";
 import Card from "./Card";
 import axios from "axios";
-import { IoWarning } from "react-icons/io5";
 
 function Blogs() {
     const [loading, setLoading] = useState(false);
     const [search, setSearch] = useState("");
     const [blogs, setBlogs] = useState([]);
     const [error, setError] = useState(null);
+
     const handleSearch = () => {
         const searchInput = document.getElementById("searchInput");
         if (searchInput) {
             setSearch(searchInput.value);
         }
     };
-    const filteredblogs = blogs.filter((course) => {
+
+    const filteredBlogs = blogs.filter((blog) => {
         const matchesSearch =
-            !search ||
-            course.Title.toLowerCase().includes(search.toLowerCase());
+            !search || blog.Title.toLowerCase().includes(search.toLowerCase());
         return matchesSearch;
     });
+
     const fetchBlogs = async () => {
         setLoading(true);
-
         try {
             const response = await axios.get("http://localhost:3000/Blogs", {
                 withCredentials: true,
@@ -32,22 +31,26 @@ function Blogs() {
             });
 
             if (response.status === 200) {
-                setBlogs(response.data);
+                const { totalPages, blogs } = response.data;
+                setBlogs(blogs);
             } else {
                 setError(response.data);
             }
         } catch (error) {
             setError(error);
         } finally {
-            setLoading(false); // Set loading state to false regardless of success or failure
+            setLoading(false);
         }
     };
+
     useEffect(() => {
         fetchBlogs();
     }, []);
+
     if (error) {
         return <ErrorPage />;
     }
+
     return (
         <div>
             {loading ? (
@@ -55,53 +58,46 @@ function Blogs() {
                     <span className="loader"></span>
                 </div>
             ) : (
-                <div className="h-screen  overflow-auto custom-overflow pt-[90px]">
-                    <div className=" flex flex-col md:flex-row justify-between items-center mx-16 mb-2 font-semibold text-gray w-300px">
-                        <div className=" text-2xl w-screen md:w-fit text-center lg:text-3xl mb-2 ">
-                            <span className=" text-green2">Read </span>
+                <div className="h-screen overflow-auto custom-overflow pt-[90px]">
+                    <div className="flex flex-col md:flex-row justify-between items-center mx-16 mb-2 font-semibold text-gray w-300px">
+                        <div className="text-2xl w-screen md:w-fit text-center lg:text-3xl mb-2 ">
+                            <span className="text-green2">Read </span>
                             Skate Blogs
                         </div>
-                        {/* Search bar  */}
-                        <div className=" flex border-2 md:mr-10">
+                        <div className="flex border-2 md:mr-10">
                             <input
                                 type="text"
-                                className=" pl-2 py-1 w-[150px] md:w-[300px]  focus:outline-none"
+                                className="pl-2 py-1 w-[150px] md:w-[300px] focus:outline-none"
                                 id="searchInput"
                             />
                             <button
-                                className=" px-2 border-l-2"
+                                className="px-2 border-l-2"
                                 onClick={handleSearch}
                             >
-                                <IoSearchOutline className=" text-2xl" />
+                                <IoSearchOutline className="text-2xl" />
                             </button>
                         </div>
                     </div>
 
                     <div className="">
-                        {blogs.length == 0 ? (
-                            <div className=" w-[80%] m-auto h-fit text-center pt-6 flex gap-1 text-2xl justify-center items-center text-gray">
+                        {blogs.length === 0 ? (
+                            <div className="w-[80%] m-auto h-fit text-center pt-6 flex gap-1 text-2xl justify-center items-center text-gray">
                                 <IoWarning />
-                                No Events Founded
+                                No Blogs Found
                             </div>
                         ) : search === "" ? (
                             blogs.map((blog) => (
-                                <div
-                                    key={blog._id}
-                                    className="w-[90vw] m-auto "
-                                >
+                                <div key={blog._id} className="w-[90vw] m-auto">
                                     <Card blog={blog} />
                                 </div>
                             ))
-                        ) : filteredblogs.length === 0 ? (
+                        ) : filteredBlogs.length === 0 ? (
                             <div className="text-center text-gray-500">
                                 No blogs match the selected filter.
                             </div>
                         ) : (
-                            filteredblogs.map((blog) => (
-                                <div
-                                    key={blog._id}
-                                    className="w-[80vw] m-auto "
-                                >
+                            filteredBlogs.map((blog) => (
+                                <div key={blog._id} className="w-[80vw] m-auto">
                                     <Card blog={blog} />
                                 </div>
                             ))
--- src/Components/Blogs/Card.jsx
+++ src/Components/Blogs/Card.jsx
@@ -5,9 +5,9 @@ function Card({ blog }) {
     return (
         <Link
             to={`/Blogs/${blog._id}`}
-            className="w-full relative rounded overflow-hidden border-b border-gray py-5 px-5 flex shrink-0 justify-start h-[300px]"
+            className="w-full h-fit relative rounded overflow-hidden border-b border-gray py-5 px-5 flex shrink-0 justify-start "
         >
-            <img className=" w-[30%] object-cover" src={img} alt={blog.Title} />
+            <img className=" w-[40%] h-[50%]  md:h-full object-cover" src={img} alt={blog.Title} />
             <div className="w-[60%] pl-6 py-4 ">
                 {/* <div className="font-bold text-xl mb-2 overflow-hidden w-[550px]">
                     {blog.Title}
