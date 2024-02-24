62cc3ddde42b3e18f108cc104a71498a928050be
imadbenmadi <imadbenmadi891@gmail.com>
2024-02-24 16:25:35 +0100

    fixing services Component

--- src/Components/Servicecs/Explore/ExploreServices.jsx
+++ src/Components/Servicecs/Explore/ExploreServices.jsx
@@ -1,43 +1,67 @@
 import React from "react";
 import Card from "./Card";
 import { Link } from "react-router-dom";
+import { IoWarning } from "react-icons/io5";
 function ExploreServices({ search, filter, services }) {
-    const filteredservices = services.filter((service) => {
-        const matchesSearch =
-            !search ||
-            service.Title.toLowerCase().includes(search.toLowerCase());
-        const matchesFilter =
-            !filter || filter.length === 0 || filter.includes(service.Category);
-        return matchesSearch && matchesFilter;
-    });
-    // If filter is null or empty, return all services
-    if ((!filter || filter.length === 0) && search === "") {
-        return (
-            <div className=" ">
-                {services.map((service, index) => (
-                    <div key={index} className="w-full ">
-                        <Card service={service} />
+    if (services.services && Array.isArray(services.services)) {
+        if (services.services.length === 0)
+            return (
+                <div className=" flex text-gray items-center gap-2 p-3">
+                    <IoWarning className=" text-2xl" />
+                    <div className="text-center text-gray">
+                        No services for the moment
                     </div>
-                ))}
+                </div>
+            );
+        const filteredservices = services.services.filter((service) => {
+            const matchesSearch =
+                !search ||
+                !service.Title ||
+                service.Title.toLowerCase().includes(search.toLowerCase());
+            const matchesFilter =
+                !filter ||
+                filter.length === 0 ||
+                !service.Category ||
+                filter.includes(service.Category);
+            return matchesSearch && matchesFilter;
+        });
+
+        if ((!filter || filter.length === 0) && search === "") {
+            return (
+                <div className=" ">
+                    {services.services.map((service, index) => (
+                        <div key={index} className="w-full ">
+                            <Card service={service} />
+                        </div>
+                    ))}
+                </div>
+            );
+        }
+
+        return (
+            <div>
+                {filteredservices.length === 0 ? (
+                    <div className="text-center text-gray-500">
+                        No services match the selected filter.
+                    </div>
+                ) : (
+                    filteredservices.map((service, index) => (
+                        <div key={index} className="w-full ">
+                            <Card service={service} />
+                        </div>
+                    ))
+                )}
             </div>
         );
     }
-
     return (
-        <div>
-            {filteredservices.length === 0 ? (
-                <div className="text-center text-gray-500">
-                    No services match the selected filter.
-                </div>
-            ) : (
-                filteredservices.map((service, index) => (
-                    <div key={index} className="w-full ">
-                        <Card service={service} />
-                    </div>
-                ))
-            )}
+        <div className=" flex text-gray items-center gap-2 p-3">
+            <IoWarning className=" text-2xl" />
+            <div className="text-center text-gray">
+                No services for the moment
+            </div>
         </div>
-    );
+    ); 
 }
 
 export default ExploreServices;
