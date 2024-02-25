6a1d66b6af111354c310600b996a2b5c7c3ba04e
imadbenmadi <imadbenmadi891@gmail.com>
2024-02-25 18:06:56 +0100

    init Courses Component

--- src/Dashboard/Dashboard_Courses/Dashboard_Courses.jsx
+++ src/Dashboard/Dashboard_Courses/Dashboard_Courses.jsx
@@ -1,17 +1,54 @@
-import React from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import ErrorPage from "../../Components/ErrorPage";
 
 function Dashboard_Courses() {
-  return (
-      <div
-          class="scrollbar-thumb-rounded-full 
-      scrollbar-track-rounded-full  scrollbar-thin
-       scrollbar-thumb-slate-700 scrollbar-track-slate-300
-       
-        h-32 overflow-y-scroll"
-      >
-          <div class="h-64 bg-slate-400"></div>
-      </div>
-  );
+    const [Courses, setCourses] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const fetch_courses = async () => {
+        setLoading(true);
+        try {
+            const response = await axios.get(
+                "http://localhost:3000/Courses",
+                {
+                    withCredentials: true,
+                    validateStatus: () => true,
+                }
+            );
+
+            if (response.status == 200) {
+                setCourses(response.data);
+            } else {
+                setError(response.data);
+            }
+        } catch (error) {
+            setError(error);
+        }
+        setLoading(false);
+    };
+    useEffect(() => {
+        fetch_courses();
+    }, []);
+    if (loading)
+        return (
+            <div className=" w-[100%] h-[200px] flex items-center justify-center">
+                <span className="loader"></span>
+            </div>
+    );
+  if (error) {
+    return <ErrorPage />;
+  }
+  
+    return (
+        <div>
+            <div className=" text-center text-gray font-semibold text-2xl my-5">
+                Skate Courses :
+            </div>
+        </div>
+    );
 }
 
-export default Dashboard_Courses
+export default Dashboard_Courses;
