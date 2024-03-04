5003320dbd39dd8dc88b063d31e8087b90bcc1e3
imadbenmadi <imadbenmadi891@gmail.com>
2024-03-04 18:45:20 +0100

    fixing hosting poblemes

--- src/Components/Home/Hero.jsx
+++ src/Components/Home/Hero.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import wallpaper1 from "../../../public/wallpaper.jpg";
 import wallpaper2 from "../../../public/wllpaper2.jpg";
+import img from "../../../public/Rectangle.png";
 import { Link } from "react-router-dom";
 import { GrFormNextLink } from "react-icons/gr";
 export default function Hero() {
@@ -9,8 +10,12 @@ export default function Hero() {
             <div className=" h-[96%] max-h-screen translate-y-[5%]    ">
                 {/* Set the image as a background */}
                 <div
-                    className={`relative  w-full h-full  bg-cover bg-center flex justify-center items-center gap-10   bg-[url('../../../public/Rectangle.png')] text-black_text`}
+                    className={`relative  w-full h-full  bg-cover bg-center flex justify-center items-center gap-10   bg-[url('${img}')] text-black_text`}
                 >
+                    <img
+                        src={`${img}`}
+                        className="z-5 absolute w-full h-full"
+                    ></img>{" "}
                     <div className="bg-black z-10 opacity-70 absolute w-full h-full"></div>{" "}
                     <div className=" absolute  font-semibold text-6xl top-24 text-gray font-serif"></div>
                     <div className="flex z-50  flex-col md:items-center w-screen md:w-[600px]">
