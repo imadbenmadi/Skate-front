caa1c1093f0108e96771dd9dd8ed31115e29e3e0
imadbenmadi <imadbenmadi891@gmail.com>
2024-03-06 15:29:46 +0100

    fixing Card UI

--- src/Dashboard/Dashboard_Courses/Current_Courses.jsx
+++ src/Dashboard/Dashboard_Courses/Current_Courses.jsx
@@ -5,6 +5,9 @@ import img from "../../../public/wallpaper.jpg";
 import { useOutletContext } from "react-router";
 import { Link } from "react-router-dom";
 import { FaPlus } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
+import { MdEdit } from "react-icons/md";
+
 function Current_Courses() {
     const Courses = useOutletContext();
     if (!Courses) return null;
@@ -45,39 +48,53 @@ function Current_Courses() {
                         </>
                     </Link>
                 </div>
-
-                <div className="flex gap-4 w-full ">
+                <div className=" ">
                     {Courses.map((item, index) => (
-                        <div key={index} className="w-full ">
-                            <div className="w-full relative rounded overflow-hidden border-b border-gray py-5 px-5 flex shrink-0 justify-start h-fit">
+                        <div
+                            key={index}
+                            className="w-full  flex border-b border-b-gray "
+                        >
+                            <div className="w-[90%]  relative  overflow-hidden  py-5 px-5 flex shrink-0 justify-start h-fit">
                                 <img
                                     className=" w-[30%] object-cover"
                                     src={img}
                                     alt={item.Title}
                                 />
-                                <div className="w-[60%] pl-6 py-4 ">
-                                    {item.Title && (
-                                        <p className="font-bold text-xl mb-2 overflow-hidden">
-                                            {item.Title}
-                                        </p>
-                                    )}
+                                <div className="w-[70%] pl-6 py-4 flex">
+                                    <div className=" w-[90%]">
+                                        {item.Title && (
+                                            <p className="font-bold text-xl mb-2 overflow-hidden">
+                                                {item.Title}
+                                            </p>
+                                        )}
 
-                                    {item.Text && (
-                                        <p className="text-gray-700 text-base">
-                                            {item.Text}
+                                        {item.Text && (
+                                            <p className="text-gray-700 text-base">
+                                                {item.Text}
+                                            </p>
+                                        )}
+                                        {item.Description && (
+                                            <p className="text-gray-700 text-base">
+                                                {item.Description}
+                                            </p>
+                                        )}
+                                        <p className="text-gray-700 text-sm pt-4">
+                                            {item.Category}
                                         </p>
-                                    )}
-                                    {item.Description && (
-                                        <p className="text-gray-700 text-base">
-                                            {item.Description}
-                                        </p>
-                                    )}
-                                    <p className="text-gray-700  text-sm absolute top-10 right-5 ">
+                                    </div>
+
+                                    <p className="text-gray text-center  text-sm top-10 right-5  w-[10%] ">
                                         {item.Price} DA
                                     </p>
-                                    <p className="text-gray-700 text-sm pt-4">
-                                        {item.Category}
-                                    </p>
+                                </div>
+                            </div>
+                            <div className=" w-[10%] flex flex-col items-center justify-start pt-6 gap-4">
+                                <div className="flex items-center justify-start gap-2 text-white  text-xl bg-green px-2 py-1 rounded w-[100px]  ">
+                                    <MdEdit /> Edit
+                                </div>
+                                <div className=" flex items-center justify-start gap-2 text-white bg-red-600 text-xl  px-2 py-1 rounded  w-[100px]">
+                                    <MdDelete />
+                                    Delete
                                 </div>
                             </div>
                         </div>
