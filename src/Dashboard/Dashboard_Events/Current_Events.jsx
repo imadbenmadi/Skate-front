cdeae1dfc36b648cdc6cbb5323413617a51066f1
imadbenmadi <imadbenmadi891@gmail.com>
2024-03-14 04:20:07 +0100

    Cahngin Mounting Stratigy

--- src/Dashboard/Dashboard_Events/Current_Events.jsx
+++ src/Dashboard/Dashboard_Events/Current_Events.jsx
@@ -2,12 +2,43 @@ import React from "react";
 import { IoWarning } from "react-icons/io5";
 import { Link } from "react-router-dom";
 import { FaPlus } from "react-icons/fa";
-import { useOutletContext } from "react-router";
 import Current_Events_Card from "./Current_Events_Card";
-
+import { useState, useEffect } from "react";
+import axios from "axios";
+import ErrorPage from "../../Components/ErrorPage";
 function Current_Events() {
-    const Events = useOutletContext();
-
+    const [Events, setEvents] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const fetch_Events = async () => {
+        setLoading(true);
+        try {
+            const response = await axios.get("http://localhost:3000/events", {
+                withCredentials: true,
+                validateStatus: () => true,
+            });
+            if (response.status == 200) {
+                setEvents(response.data.events);
+            } else {
+                setError(response.data);
+            }
+        } catch (error) {
+            setError(error);
+        }
+        setLoading(false);
+    };
+    useEffect(() => {
+        fetch_Events();
+    }, []);
+    if (loading)
+        return (
+            <div className=" w-[100%] h-[200px] flex items-center justify-center">
+                <span className="loader"></span>
+            </div>
+        );
+    if (error) {
+        return <ErrorPage />;
+    }
     if (!Events) return null;
     else if (Events.length === 0)
         return (
--- src/Dashboard/Dashboard_Events/Dashboard_Events.jsx
+++ src/Dashboard/Dashboard_Events/Dashboard_Events.jsx
@@ -1,44 +1,10 @@
 import React from "react";
-import { useState, useEffect } from "react";
-import axios from "axios";
-import ErrorPage from "../../Components/ErrorPage";
+
 import { Outlet } from "react-router-dom";
 function Dashboard_Events() {
-    const [Events, setEvents] = useState(null);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
-    const fetch_Events = async () => {
-        setLoading(true);
-        try {
-            const response = await axios.get("http://localhost:3000/events", {
-                withCredentials: true,
-                validateStatus: () => true,
-            });
-          if (response.status == 200) {
-                setEvents(response.data.events);
-            } else {
-                setError(response.data);
-            }
-        } catch (error) {
-            setError(error);
-        }
-        setLoading(false);
-    };
-    useEffect(() => {
-        fetch_Events();
-    }, []);
-    if (loading)
-        return (
-            <div className=" w-[100%] h-[200px] flex items-center justify-center">
-                <span className="loader"></span>
-            </div>
-        );
-    if (error) {
-        return <ErrorPage />;
-    }
     return (
         <div className=" pt-8">
-            <Outlet context={Events} />
+            <Outlet  />
         </div>
     );
 }
