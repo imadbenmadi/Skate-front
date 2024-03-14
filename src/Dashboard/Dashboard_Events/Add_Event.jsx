742dd5ed581015576897eac3431e0a43d0020dd0
imadbenmadi <imadbenmadi891@gmail.com>
2024-03-14 04:18:01 +0100

    integratting Add Event

--- src/Dashboard/Dashboard_Events/Add_Event.jsx
+++ src/Dashboard/Dashboard_Events/Add_Event.jsx
@@ -23,8 +23,6 @@ function Add_Event() {
                         Title: "",
                         Text: "",
                         Description: "",
-                        Price: "",
-                        Category: "",
                     }}
                     validate={(values) => {
                         const errors = {};
@@ -40,32 +38,77 @@ function Add_Event() {
                         if (!values.Description) {
                             errors.Description = "Description is required.";
                         }
-                        // Validate Price
-                        if (!values.Price) {
-                            errors.Price = "Price is required.";
-                        } else if (isNaN(values.Price))
-                            errors.Price = "Invalid Price";
-
-                        // Validate Category
-                        if (!values.Category) {
-                            errors.Category = "Category is required.";
-                        }
                         return errors;
                     }}
                     onSubmit={async (values, { setSubmitting, resetForm }) => {
                         try {
                             setSubmitting(true);
-                            // Call your Axios POST request here
-                            // Example:
-                            // const response = await Axios.post("your_api_endpoint", values);
-                            // Handle response accordingly
-                            // Example:
-                            // if (response.status === 200) {
-                            //     resetForm();
-                            //     Swal.fire("Success!", "Event added successfully.", "success");
-                            // } else {
-                            //     Swal.fire("Error!", "Failed to add Event.", "error");
-                            // }
+
+                            let response = await Axios.post(
+                                "http://localhost:3000/Dashboard/Events",
+                                values,
+                                {
+                                    withCredentials: true,
+                                    validateStatus: () => true,
+                                }
+                            );
+                            if (response.status == 200) {
+                                resetForm();
+                                Swal.fire(
+                                    "Done!",
+                                    "Event has been created Successfully",
+                                    "success"
+                                );
+                            } else if (response.status == 404) {
+                                Swal.fire(
+                                    "Error",
+                                    `${response.data.message}`,
+                                    "error"
+                                );
+                            } else if (response.status == 400) {
+                                Swal.fire(
+                                    "Error!",
+                                    `Internal server error : ${response.data.message}`,
+                                    "error"
+                                );
+                            } else if (response.status == 401) {
+                                Swal.fire({
+                                    title: "Unauthorised Action",
+                                    text: "You should Login again ",
+                                    icon: "error",
+                                    confirmButtonColor: "#3085d6",
+
+                                    confirmButtonText: "Go to Admin Login Page",
+                                }).then((result) => {
+                                    if (result.isConfirmed) {
+                                        Navigate("/Dashboard_Login");
+                                    }
+                                });
+                            } else if (response.status == 409) {
+                                Swal.fire(
+                                    "Error!",
+                                    `${response.data.message}`,
+                                    "error"
+                                );
+                            } else if (response.status == 429) {
+                                Swal.fire(
+                                    "Error!",
+                                    `Too many Requests , ${response.data.message}`,
+                                    "error"
+                                );
+                            } else if (response.status == 500) {
+                                Swal.fire(
+                                    "Error!",
+                                    `Internal server error : ${response.data.message}`,
+                                    "error"
+                                );
+                            } else {
+                                Swal.fire(
+                                    "Error!",
+                                    `Something Went Wrong. Please try again , ${response.data.message}`,
+                                    "error"
+                                );
+                            }
                         } catch (error) {
                             Swal.fire(
                                 "Error!",
@@ -138,46 +181,7 @@ function Add_Event() {
                                     style={errorInputMessage}
                                 />
                             </div>
-                            <div className=" flex gap-3 items-center justify-center w-[70%]">
-                                <div>
-                                    <div>
-                                        Price{" "}
-                                        <span className="text-red-600 font-semibold">
-                                            *
-                                        </span>
-                                    </div>
-                                    <Field
-                                        type="text"
-                                        name="Price"
-                                        className="border border-gray_white px-2 py-1 rounded shadow-sm"
-                                        disabled={isSubmitting}
-                                    />
-                                    <ErrorMessage
-                                        name="Price"
-                                        component="div"
-                                        style={errorInputMessage}
-                                    />
-                                </div>
-                                <div>
-                                    <div>
-                                        Category{" "}
-                                        <span className="text-red-600 font-semibold">
-                                            *
-                                        </span>
-                                    </div>
-                                    <Field
-                                        type="text"
-                                        name="Category"
-                                        className="border border-gray_white px-2 py-1 rounded shadow-sm"
-                                        disabled={isSubmitting}
-                                    />
-                                    <ErrorMessage
-                                        name="Category"
-                                        component="div"
-                                        style={errorInputMessage}
-                                    />
-                                </div>
-                            </div>
+                            
 
                             <button
                                 type="submit"
