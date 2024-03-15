db45648adc9b54c5b0ed552ddb7a59f45dfda750
imadbenmadi <imadbenmadi891@gmail.com>
2024-03-15 17:22:27 +0100

    fixing ui

--- src/Components/Blogs/Card.jsx
+++ src/Components/Blogs/Card.jsx
@@ -1,36 +1,45 @@
 import React from "react";
 import img from "../../../public/wallpaper.jpg";
 import { Link } from "react-router-dom";
+import { Formate_Date } from "../../Logic/Formate_Date"; // Import your date formatting function
+
 function Card({ blog }) {
     return (
         <Link
-            to={`/Blogs/${blog._id}`}
-            className="select-none w-full h-fit relative rounded overflow-hidden border-b border-gray py-5 px-5 flex shrink-0 justify-start "
+            to={`/blogs/${blog._id}`}
+            className="select-none w-full relative  overflow-hidden border-b border-gray py-5 flex shrink-0 justify-start h-fit"
         >
             <img
-                className=" w-[40%] h-[50%]  md:h-full object-cover"
+                className="w-[30%] object-cover h-[120px]"
                 src={img}
                 alt={blog.Title}
             />
-            <div className="w-[60%] pl-6 py-4 ">
-                {/* <div className="font-bold text-xl mb-2 overflow-hidden w-[550px]">
-                    {blog.Title}
-                </div> */}
+            <div className="w-[60%] pl-6 ">
                 {blog.Title && (
-                    <p className="font-bold text-xl mb-2 overflow-hidden">
-                        {blog.Title.slice(0, 60) +
-                            (blog.Title.length > 60 ? "..." : "")}
+                    <p className="font-bold  text-lg md:text-xl  overflow-hidden">
+                        {window.innerWidth > 640
+                            ? blog.Title.slice(0, 60)
+                            : blog.Title.slice(0, 30)}
+                        {blog.Title.length >
+                        (window.innerWidth > 640 ? 60 : 30)
+                            ? "..."
+                            : ""}
                     </p>
                 )}
-
                 {blog.Text && (
                     <p className="text-gray text-base">
-                        {blog.Text.slice(0, 150) +
-                            (blog.Text.length > 150 ? "..." : "")}
+                        {window.innerWidth > 640
+                            ? blog.Text.slice(0, 150)
+                            : blog.Text.slice(0, 75)}
+                        {blog.Text.length >
+                        (window.innerWidth > 640 ? 150 : 75)
+                            ? "..."
+                            : ""}
                     </p>
                 )}
-
-                <p className="text-gray text-sm pt-4">{blog.Category}</p>
+                <p className="text-gray text-sm pt-2 font-semibold">
+                    {Formate_Date(blog.Date)}
+                </p>
             </div>
         </Link>
     );
