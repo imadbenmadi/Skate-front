706d562b8208dc3e28ffb9e1a3d8d847c54ab407
imadbenmadi <imadbenmadi891@gmail.com>
2024-04-14 22:36:03 +0100

    .

--- src/Languages/Arabic/Logic/Formate_Date.js
+++
@@ -1,8 +0,0 @@
-import { format } from "date-fns";
-
-export const Formate_Date = (date) => {
-    return format(date, "d MMMM yyyy HH:mm");
-};
-export const Formate_Date_NoYear = (date) => {
-    return format(date, "d MMMM HH:mm");
-};
